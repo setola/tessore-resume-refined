aef7a88e06809e296eb0c5359d6c4ea32b02f4c7
it <it@dmind.it>
2025-06-30 16:49:27 +0200

    wip

--- src/data/resume.ts
+++ src/data/resume.ts
@@ -64,12 +64,12 @@ export interface ResumeData {
 export const resumeData: ResumeData = {
   "basics": {
     "name": "Emanuele Tessore",
-    "label": "IT Nerd",
+    "label": "Tech Enthusiast",
     "picture": "https://www.gravatar.com/avatar/0a45f1fb68f93a8a28bac085c505eacd?size=800",
     //"image": "https://www.gravatar.com/avatar/0a45f1fb68f93a8a28bac085c505eacd?size=800",
     "email": "emanuele.tessore@gmail.com",
     "website": "https://www.emanueletessore.com/",
-    "summary": "Hi! my name is **Emanuele** but everybody calls me **Tex** or **Setola**.\n\nI'm a **dev ops**, I was a **web developer** and I'll forever be a **tech enthusiast**.\n\nI've started developing applications in academia, in early 2000, with languages like **C++** and **Java**.\n\nI've always been fascinated by the potential of the Internet, a passion that led me to face **either technical** or **design and marketing** topics.\n\nI also developed a strong interest in automation and interactions between technology and the physical world while exploring micro-controllers, IoT and CNC stuff\n\nTechnology not only is the key point of my professional career, but it’s also my oldest hobby and of course one **passion** that takes me over the years.\n\nI like Linux, OpenSource, Arduino and Raspberry PI, Kubernetes, motorbikes and drums",
+    "summary": "Hi! my name is **Emanuele** but everybody calls me **Tex** or **Setola**.\n\nI'm a **DevOps Manager** and **System Architect** with a particular focus on **Cyber Security**; I was a **Web Developer** and an **IT Manager**.\n\nI've started developing applications in early 2000, with languages like **C++** and **Java**. Then I've followed the web standard with **PHP** ans **Node**, with a little trip to **GoLang**\n\nI've always been fascinated by the potential of the Internet, a passion that led me to face **either technical** or **design and marketing** topics.\n\nI also developed a strong interest in **automation** and technology interactions with the physical world while exploring micro-controllers, IoT and CNC stuff\n\nTechnology is both the key point of my professional career and my oldest hobby and of course one **passion** that takes me over the years.\n\nI like **Linux**, **OpenSource**, **Arduino** and **Raspberry PI**, **Kubernetes**, motorbikes and drums",
     "location": {
       "countryCode": "IT",
       "region": "Venice"
